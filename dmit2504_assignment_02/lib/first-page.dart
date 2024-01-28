644ebe65f34ad4e8a33de3942f74df607e702381
TompyxP <cntompkins245@gmail.com>
2024-01-27 23:32:45 -0700

    assignment 2 "done", seems the instructions and theme may need some adjustments

--- dmit2504_assignment_02/lib/first-page.dart
+++ dmit2504_assignment_02/lib/first-page.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: avoid_print, use_key_in_widget_constructors, file_names, todo, prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import
 
+import 'dart:math';
+
+import 'package:dmit2504_assignment_02/main.dart';
 import 'package:flutter/material.dart';
 import './widgets/mysnackbar.dart';
 
@@ -36,16 +39,67 @@ class MyFirstPageState extends State<MyFirstPage> {
             children: <Widget>[
               //TODO: Build the label and switch here
               //as children of the row.
-              
+              Text('Enable Buttons'),
+              Switch(
+                value: enabled,
+                onChanged: (value) {
+                  setState(() {
+                    enabled = value;
+                  });
+                },
+              ),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               //TODO: Build the two buttons here as children of the row.
-              // For each button use a "Visibility Widget" and its child 
+              // For each button use a "Visibility Widget" and its child
               // will be an "ElevatedButton"
-              
+              Visibility(
+                  visible: enabled,
+                  child: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(8),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            padding:
+                                MaterialStateProperty.all(EdgeInsets.all(20))),
+                        onPressed: () {
+                          setState(() {
+                            timesClicked++;
+                          });
+                        },
+                        child: Text(timesClicked == 0
+                            ? 'Click Me'
+                            : 'Clicked $timesClicked'),
+                      ))),
+              Visibility(
+                  visible: enabled,
+                  child: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(8),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            padding:
+                                MaterialStateProperty.all(EdgeInsets.all(20))),
+                        onPressed: () {
+                          setState(() {
+                            timesClicked = 0;
+                          });
+                        },
+                        child: Text('Reset'),
+                      ))),
             ],
           ),
           const SizedBox(
@@ -63,7 +117,88 @@ class MyFirstPageState extends State<MyFirstPage> {
                   // a submit button that will show a
                   // snackbar with the "firstName" if validation
                   // is satisfied.
-                  
+                  TextFormField(
+                    controller: textEditingController,
+                    maxLength: 10,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.hourglass_top),
+                      helperText: 'min 1, max 10',
+                      label: Text('first name'),
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      suffixIcon: Icon(Icons.check_circle),
+                    ),
+                    validator: (value) {
+                      return value!.length > 10
+                          ? 'Must be less than 10 characters'
+                          : value.isEmpty
+                              ? 'Must be more than 1 character'
+                              : null;
+                    },
+                    onSaved: (value) {
+                      firstName = value;
+                    },
+                  ),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              padding: MaterialStateProperty.all(
+                                  EdgeInsets.only(
+                                      top: 5, bottom: 5, left: 20, right: 20))),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              formKey.currentState!.save();
+                              scaffoldMessengerKey.currentState?.showSnackBar(
+                                SnackBar(
+                                  duration: const Duration(seconds: 5),
+                                  behavior: SnackBarBehavior.floating,
+                                  content: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.favorite,
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        'Hey There, Your name is $firstName',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15.0),
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      TextButton(
+                                          style: ButtonStyle(
+                                              foregroundColor:
+                                                  MaterialStateProperty.all(
+                                                      Colors.white),
+                                              backgroundColor:
+                                                  MaterialStateProperty.all(
+                                                      Color(0xFF442B2D))),
+                                          onPressed: () => print('A message'),
+                                          child: Text(
+                                            'Click Me',
+                                            style: TextStyle(fontSize: 15.0),
+                                          ))
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ))
                 ],
               ),
             ),
